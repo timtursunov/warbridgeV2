a79945458a7e298ea8009874bc95581557c0a8b3
timtursunov <timtursgram@gmail.com>
2021-08-31 00:55:09 -0400

    updated modal

--- app.js
+++ app.js
@@ -19,9 +19,7 @@ navBurger.addEventListener('click', ()=> {
     closeIcon.classList.toggle('header-burger__close--clicked')
 
 })
-
 // iphone draggable carousel
-
 $('.owl-carousel').owlCarousel({   
     loop: false,   
     autoWidth:true, 
@@ -185,7 +183,6 @@ let nextSlide = () => {
         console.log('other')
     }
 }
-
 // for large laptop 
 let previousSlideLaptop = () => {
     currentSlide = currentSlide - 1;
@@ -236,7 +233,7 @@ let nextSlideLaptop = () => {
             textTwo.classList.add('slide__text--next')
             slideBtn2.classList.add('slide__button--next')
 
-            let unit = -currentSlide*1057
+            let unit = -currentSlide*1058
             let leftPosition = unit+'px'
             holder.style.left = leftPosition
             console.log('it worked')
@@ -247,7 +244,7 @@ let nextSlideLaptop = () => {
             holder3.classList.remove('holder--prev')
             textThree.classList.add('slide__text--next')
             slideBtn3.classList.add('slide__button--next')
-            let unit = -currentSlide*1057
+            let unit = -currentSlide*1058
             let leftPosition = unit+'px'
             holder.style.left = leftPosition
         } else if (currentSlide === 3){
@@ -257,7 +254,7 @@ let nextSlideLaptop = () => {
             holder4.classList.remove('holder--prev')
             textFour.classList.add('slide__text--next')
             slideBtn4.classList.add('slide__button--next')
-            let unit = -currentSlide*1057
+            let unit = -currentSlide*1058
             let leftPosition = unit+'px'
             holder.style.left = leftPosition
             nextBtn.disabled= true
@@ -266,8 +263,6 @@ let nextSlideLaptop = () => {
         console.log('other')
     }
 }
-
-
 prevBtn.addEventListener('click', ()=> {
     screen.width > 1919 ? previousSlide() : previousSlideLaptop()
 })
@@ -275,5 +270,98 @@ nextBtn.addEventListener('click', ()=> {
     screen.width > 1919 ? nextSlide() : nextSlideLaptop()
 } )
 
+// select overlays
+let overlay1 = document.querySelector('.overlay1')
+let btnAbout1 = document.querySelector('#btnAbout1')
+let card1 = document.querySelector('.card__avatar1')
+let cardPClicked1 = document.querySelector('.card__p--clicked1')
+let cardIconBox1 = document.querySelector('.card__icon-box1')
+let cardIconOpened1 = document.querySelector('.card__icon--opened1')
+let cardPclickedSecond1 = document.querySelector('.card__p--clicked-second1')
+//card__p--clicked-second4
 
-// 
+let overlay2 = document.querySelector('.overlay2')
+let btnAbout2 = document.querySelector('#btnAbout2')
+let card2 = document.querySelector('.card__avatar2')
+let cardPClicked2 = document.querySelector('.card__p--clicked2')
+let cardIconBox2 = document.querySelector('.card__icon-box2')
+let cardIconOpened2 = document.querySelector('.card__icon--opened2')
+let cardPclickedSecond2 = document.querySelector('.card__p--clicked-second2')
+
+let overlay3 = document.querySelector('.overlay3')
+let btnAbout3 = document.querySelector('#btnAbout3')
+let card3 = document.querySelector('.card__avatar3')
+let cardPClicked3 = document.querySelector('.card__p--clicked3')
+let cardIconBox3 = document.querySelector('.card__icon-box3')
+let cardIconOpened3 = document.querySelector('.card__icon--opened3')
+let cardPclickedSecond3 = document.querySelector('.card__p--clicked-second3')
+
+let overlay4 = document.querySelector('.overlay4')
+let btnAbout4 = document.querySelector('#btnAbout4')
+let card4 = document.querySelector('.card__avatar4')
+let cardPClicked4 = document.querySelector('.card__p--clicked4')
+let cardIconBox4 = document.querySelector('.card__icon-box4')
+let cardIconOpened4 = document.querySelector('.card__icon--opened4')
+let cardPclickedSecond4 = document.querySelector('.card__p--clicked-second4')
+
+//  card__icon--opened2
+btnAbout1.addEventListener('click', ()=> {
+    card1.classList.add('card--cleaned')
+    btnAbout1.classList.add('overlay--cleaned')
+    cardIconBox1.style.display = 'flex'
+    cardPClicked1.innerHTML= 'Yazan is a professional international consultant with presence in several countries. We work hand in hand with our clients, delivering at the coalface. Together, we crack some of the largest and most complex challenges in the public and private sectors, by pinpointing. We work hand in hand with our clients, delivering at the coalface.'
+    cardPclickedSecond1.innerHTML = ' Our approach is not just about spreadsheets and mathematical skill: we help you see powerful truths about your organisation, so you know the actions we take together will be the right ones.'
+})
+cardIconOpened1.addEventListener('click', () => {
+    cardPClicked1.innerHTML = ''
+    cardPclickedSecond1.innerHTML = ''
+    cardIconBox1.style.display = 'none'
+    card1.classList.remove('card--cleaned')
+    btnAbout1.classList.remove('overlay--cleaned')
+})
+btnAbout2.addEventListener('click', ()=> {
+    card2.classList.add('card--cleaned')
+    cardPclickedSecond2.innerHTML = ''
+    btnAbout2.classList.add('overlay--cleaned')
+    cardIconBox2.style.display = 'flex'
+    cardPClicked2.innerHTML= 'Marcus is a professional international consultant with presence in several countries. We work hand in hand with our clients, delivering at the coalface. Together, we crack some of the largest and most complex challenges in the public and private sectors, by pinpointing. We work hand in hand with our clients, delivering at the coalface.'
+    cardPclickedSecond2.innerHTML = ' Our approach is not just about spreadsheets and mathematical skill: we help you see powerful truths about your organisation, so you know the actions we take together will be the right ones.'
+})
+cardIconOpened2.addEventListener('click', () => {
+    cardPClicked2.innerHTML = ''
+    cardPclickedSecond2.innerHTML = ''
+    cardIconBox2.style.display = 'none'
+    card2.classList.remove('card--cleaned')
+    btnAbout2.classList.remove('overlay--cleaned')
+})
+btnAbout3.addEventListener('click', ()=> {
+    card3.classList.add('card--cleaned')
+    btnAbout3.classList.add('overlay--cleaned')
+    cardIconBox3.style.display = 'flex'
+    cardPClicked3.innerHTML= 'Emily is a professional international consultant with presence in several countries. We work hand in hand with our clients, delivering at the coalface. Together, we crack some of the largest and most complex challenges in the public and private sectors, by pinpointing. We work hand in hand with our clients, delivering at the coalface.'
+    cardPclickedSecond3.innerHTML = 'Our approach is not just about spreadsheets and mathematical skill: we help you see powerful truths about your organisation, so you know the actions we take together will be the right ones.'
+})
+cardIconOpened3.addEventListener('click', () => {
+    cardPClicked3.innerHTML = ''
+    cardPclickedSecond3.innerHTML = ''
+    cardIconBox3.style.display = 'none'
+    card3.classList.remove('card--cleaned')
+    btnAbout3.classList.remove('overlay--cleaned')
+})
+btnAbout4.addEventListener('click', ()=> {
+    card4.classList.add('card--cleaned')
+    btnAbout4.classList.add('overlay--cleaned')
+    cardIconBox4.style.display = 'flex'
+    cardPClicked4.innerHTML= 'Marcus is a professional international consultant with presence in several countries. We work hand in hand with our clients, delivering at the coalface. Together, we crack some of the largest and most complex challenges in the public and private sectors, by pinpointing. We work hand in hand with our clients, delivering at the coalface.'
+    cardPclickedSecond4.innerHTML = ' Our approach is not just about spreadsheets and mathematical skill: we help you see powerful truths about your organisation, so you know the actions we take together will be the right ones.'
+})
+cardIconOpened4.addEventListener('click', () => {
+    cardPClicked4.innerHTML = ''
+    cardPclickedSecond4.innerHTML = ''
+    cardIconBox4.style.display = 'none'
+    card4.classList.remove('card--cleaned')
+    btnAbout4.classList.remove('overlay--cleaned')
+})
+
+
+
